1e6a005133609f2a67d00d68994782125a7c1cc9
Prandi Fabiel Cortorreal Reynoso <prandi_cortorreal@ucne.edu.do>
2022-02-08 23:38:24 -0400

    Programando la consulta de clientes.

--- UI/Consultas/cClientes.xaml.cs
+++ UI/Consultas/cClientes.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Proyecto_Seguro_PA1.Entidades;
+using Proyecto_Seguro_PA1.BLL; 
 
 namespace Proyecto_Seguro_PA1.UI.Consultas
 {
@@ -26,7 +28,119 @@ namespace Proyecto_Seguro_PA1.UI.Consultas
 
         private void BuscarIDButton_Click(object sender, RoutedEventArgs e)
         {
+            var listado = new List<Clientes>();
 
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        try
+                        {
+                            if (Desde_DataPicker.SelectedDate != null)
+                                listado = ClientesBLL.GetList(
+                                    c => c.FechaRegistro.Date >= Desde_DataPicker.SelectedDate &&
+                                    c.FechaRegistro.Date <= Hasta_DataPicker.SelectedDate &&
+                                    c.ClienteId == Utilidades.ToInt(CriterioTextBox.Text)
+                                );
+                            else
+                                listado = ClientesBLL.GetList(e => e.ClienteId == Utilidades.ToInt(CriterioTextBox.Text));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    case 1:
+                        try
+                        {
+                            if (Desde_DataPicker.SelectedDate != null)
+                                listado = ClientesBLL.GetList(
+                                    c => c.FechaRegistro.Date >= Desde_DataPicker.SelectedDate &&
+                                    c.FechaRegistro.Date <= Hasta_DataPicker.SelectedDate &&
+                                    c.Nombre.ToLower().Contains(CriterioTextBox.Text.ToLower())
+                                );
+                            else
+                                listado = ClientesBLL.GetList(d => d.Nombre.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            if (Desde_DataPicker.SelectedDate != null)
+                                listado = ClientesBLL.GetList(
+                                    c => c.FechaRegistro.Date >= Desde_DataPicker.SelectedDate &&
+                                    c.FechaRegistro.Date <= Hasta_DataPicker.SelectedDate &&
+                                    c.Identificacion.ToLower().Contains(CriterioTextBox.Text.ToLower())
+                                );
+                            else
+                                listado = ClientesBLL.GetList(d => d.Identificacion.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            if (Desde_DataPicker.SelectedDate != null)
+                                listado = ClientesBLL.GetList(
+                                    c => c.FechaRegistro.Date >= Desde_DataPicker.SelectedDate &&
+                                    c.FechaRegistro.Date <= Hasta_DataPicker.SelectedDate &&
+                                    c.Telefono.ToLower().Contains(CriterioTextBox.Text.ToLower())
+                                );
+                            else
+                                listado = ClientesBLL.GetList(d => d.Telefono.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    case 4:
+                        try
+                        {
+                            if (Desde_DataPicker.SelectedDate != null)
+                                listado = ClientesBLL.GetList(
+                                    c => c.FechaRegistro.Date >= Desde_DataPicker.SelectedDate &&
+                                    c.FechaRegistro.Date <= Hasta_DataPicker.SelectedDate &&
+                                    c.Email.ToLower().Contains(CriterioTextBox.Text.ToLower())
+                                );
+                            else
+                                listado = ClientesBLL.GetList(d => d.Email.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+
+                }
+            }
+            else
+            {
+                if (Desde_DataPicker.SelectedDate != null)
+                    listado = ClientesBLL.GetList(e => e.FechaRegistro.Date >= Desde_DataPicker.SelectedDate);
+
+                if (Hasta_DataPicker.SelectedDate != null)
+                    listado = ClientesBLL.GetList(e => e.FechaRegistro.Date <= Hasta_DataPicker.SelectedDate);
+
+                if (Desde_DataPicker.SelectedDate == null && Hasta_DataPicker.SelectedDate == null)
+                    listado = ClientesBLL.GetList(c => true);
+
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+
+            /*var conteo = listado.Count;
+            ConteoTextbox.Text = conteo.ToString();*/
         }
     }
 }
