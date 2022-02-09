2f372d198f60944d780fbc02a3db9322cc8ca4f0
Alvaro Hernandez <alvarostarling24@gmail.com>
2022-02-08 20:21:06 -0400

    Agregando: Contexto,entidades y instalando paquetes

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Seguro_PA1.Entidades
+{
+    public class Clientes
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Identificacion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
